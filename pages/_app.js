18408878a20bc99b03b1c6a1b3b627ec66b97fc7
Aleksey Koshkin <ekbdizzy@yandex.ru>
2022-06-11 16:41:11 +0600

    Setup project

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("../styles/globals.css");
+function MyApp({ Component, pageProps }) {
+    return React.createElement(Component, Object.assign({}, pageProps));
+}
+exports.default = MyApp;
