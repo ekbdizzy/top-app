4cb382be3f80ea95a632688a3853dbcc96e5fa5f
Aleksey Koshkin <ekbdizzy@yandex.ru>
2022-06-11 18:21:50 +0600

    Add Htag component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,13 @@
 import type {NextPage} from 'next';
+import {Htag} from "../components";
 
 
 const Home: NextPage = () => {
-    return <div>Hheheh</div>;
+    return <div>
+        <Htag tag='h1'>Header 1</Htag>
+        <Htag tag='h2'>Header 2</Htag>
+        <Htag tag='h3'>Header 3</Htag>
+    </div>;
 };
 
 export default Home;
