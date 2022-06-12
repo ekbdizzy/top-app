0127657e8f09c05fdf33aca87c9824dc42220d36
Aleksey Koshkin <ekbdizzy@yandex.ru>
2022-06-12 19:10:01 +0600

    Add component Rating Part 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,19 @@
 import type {NextPage} from 'next';
-import {Button, Htag, Tag, P} from "../components";
+import {Button, Htag, Tag, P, Rating} from "../components";
+import {useEffect, useState} from "react";
 
 
 const Home: NextPage = () => {
+    const [counter, setCounter] = useState<number>(0);
     return <div>
         <Htag tag='h1'>Header 1</Htag>
         <Htag tag='h2'>Header 2</Htag>
         <Htag tag='h3'>Header 3</Htag>
         <Htag tag='h2'>Buttons</Htag>
+        <div>
+            <span>{counter}</span>
+            <Button appearance='primary' onClick={() => setCounter(x => x + 1)}>Counter</Button>
+        </div>
         <div>
             <Button appearance='ghost' arrow='down'>132232</Button>
             <Button appearance="primary" arrow='right'>132232</Button>
@@ -18,11 +24,14 @@ const Home: NextPage = () => {
             <P size='l'>Hello, i'm a text L</P>
         </div>
         <div>
-            <hr />
+            <hr/>
             <Tag href='https://google.com'>Google</Tag>
             <Tag color='red'>Red Tag</Tag>
             <Tag size='s' color='green'>Green small tag</Tag>
-            <hr />
+            <hr/>
+        </div>
+        <div>
+            <Rating rating={4}/>
         </div>
     </div>;
 };
